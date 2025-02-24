d956c5695e04fd755abc35fd7e7f98c2e888c42b
harshbhatt7585 <harshbhatt7585@gmail.com>
2025-02-24 21:26:53 +0530

    Rotate Array

---
+++ rotate_array.py
@@ -0,0 +1,38 @@
+"""
+1. **Rotate Array to the Right by k Steps**
+
+Given an array of integers and a number k, rotate the array to the right by k  steps.
+
+**Example:**
+
+Input: [1, 2, 3, 4, 5, 6, 7],  k = 3
+
+Output: [5, 6, 7, 1, 2, 3, 4]
+"""
+
+
+
+def rorate_array(array: list, k: int):
+    for idx in range(len(array)):
+        pop_ele = array.pop(0)
+        array.append(pop_ele)
+        if idx == k:
+            break
+    return array
+
+## How I am solving it
+"""
+I am iteratinng to the length of the array, and poping(removing) 
+the first element from the list and rotating it by adding to the last
+until I reached to the index == k
+"""
+
+
+            
+if __name__ == "__main__":
+    list_ = [1, 2, 3, 4, 5, 6, 7]
+    k = 3
+    rotated_array = rorate_array(list_, k)
+    print(rotated_array)
+
+        
